9ebdc4f54b3dbda6caf72007fac3a5dbbb57b66d
Etienne Laurin <etienne@atnnn.com>
2019-07-26 22:39:22 +0000

    start build

--- win32/cmd-exe.pl
+++ win32/cmd-exe.pl
@@ -10,10 +10,9 @@ use POSIX qw(dup dup2);
 
 sub cmd {
   my ($c) = @_;
-  $c =~ s/ \\ \n / /gx;
   # print STDERR "\nprocessed cmd: ".Dumper($c);
   my $unescaped = qr/ (?<! \^ ) /nx;
-  my $wordchar = qr/( [a-zA-Z0-9\-_%=.\/#*] | \\ (?! \\* " ) | \^. )/nx;
+  my $wordchar = qr/( [a-zA-Z0-9\-_%=.\/#*,] | \\ (?! \\* " ) | \^. )/nx;
   my $quoted = qr/
     ( (\\\\) *)
     ( ( \\" )
@@ -24,25 +23,26 @@ sub cmd {
   my $operator = qr/
     $unescaped
     ( \&\&
-    | [0-9]* ( > | >> )
+    | [0-9]* ( > >? )
     | \( | \)
+    # | \\ \n
     ) /nx;
   my $token = qr/
     ( (?<op> $operator )
     | (?<word> ( $wordchar | \^. | $quoted )+ )
     )
   /nxs;
-  my $white = qr/ [ \t\r\n] * /nx;
+  my $white = qr/ ( [ \t\r\n] | \\ \n ) * /nx;
   my @tokens;
   while ($c =~ /
     ( $token
-    | (?<nope> . )
+    | (?<nope> .{1,20} )
     )
     $white
   /gxns) {
     # print STDERR "\nword: " . Dumper($+);
-    if (defined $+{nope}) { die("cannot parse: ".$c) }
-    if (defined $+{and}) { push @tokens, $+{and} }
+    if (defined $+{nope}) { die("cannot parse (at '$+{nope}'):".$c) }
+    if (defined $+{op}) { push @tokens, $+{op} }
     if (defined $+{word}) {
       my $word = $+{word};
       $word =~ s/
@@ -67,10 +67,10 @@ sub popcmd {
   my $inparen = 0;
   while ($#$cmds != -1) {
     my $next = $cmds->[0];
-    if (!$inparen && $next eq "&&") {
+    if (!$inparen && $next =~ / ^( \&\& | \\ \n )$ /nx) {
       return @ret;
     }
-    pop @$cmds;
+    shift @$cmds;
     push @ret, $next;
     if ($next eq "(") {
       $inparen++;
@@ -86,30 +86,30 @@ sub popcmd {
 }
 
 sub evalcmds {
-  # print STDERR "\nevalcmds: ".Dumper(@_);
+  print STDERR "\nevalcmds: ".Dumper(\@_);
   my @cmds = @_;
 
   my @cmd = popcmd(\@cmds);
+  print STDERR "\npopped: ".Dumper(\@cmd);
+  print STDERR "\nrest: ".Dumper(\@cmds);
 
   if ($#cmd == -1) {
     die "empty command!";
   }
 
   my $ret = 1;
-  my $savedfd;
 
-  if ($#cmd >= 1 && $cmd[$#cmd-1] =~ / (?<! \^) (?<fd> [0-9]+) (?<mode> >>?) $ /xn) {
-    my $outfile = pop @cmd;
-    pop @cmd;
+  if ($#cmd >= 1 && $cmd[$#cmd-1] =~ / (?<! \^) (?<fd> [0-9]*) (?<mode> >>?) $ /xn) {
+    my $outfile = $cmd[$#cmd];
     $outfile = "/dev/null" if $outfile =~ /^NUL$/i;
     my $infd = $+{"fd"};
     $infd = STDOUT->fileno if $infd eq "";
-    $savedfd = [$infd, dup($infd)];
+    my $savedfd = [$infd, dup($infd)];
     open(my $out, $+{mode}, $outfile) or die;
     dup2($out->fileno, $infd) or die;
-  }
-
-  if ($cmd[0] eq "(") {
+    $ret = evalcmds(@cmd[0 .. ($#cmd - 2)]);
+    dup2($savedfd->[1], $savedfd->[0]);
+  } elsif ($cmd[0] eq "(") {
     if ($cmd[$#cmd] ne ")") {
       die "missing ')' in: @cmd";
     }
@@ -117,32 +117,31 @@ sub evalcmds {
     pop @cmd;
     $ret = evalcmds(@cmd);
   } else {
-    @cmd = map { s/ \^ (.) /$1/xg } @cmd;
+    for (@cmd) { s/ \^ (.) /$1/xg }
+    $cmd[0] =~ s/ \. $ //x;
     given ($cmd[0]) {
       when (/^for$/i) { $ret = eval_for(@cmd[1 .. $#_]) }
       when (/^set$/i) { $ret = eval_set(@cmd[1 .. $#_]) }
       when (/^if$/i) { $ret = eval_if(@cmd[1 .. $#_]) }
       when (/^copy$/i) { $ret = eval_copy(@cmd[1 .. $#_]) }
+      when (/^rem$/i) { $ret = 0 }
       default {
-        $cmd[0] =~ s/ \. $ //x;
-        # print STDERR "system: ".join(" ",@cmd)."\n";
+        print STDERR "system: ".join(" ",@cmd)."\n";
         $ret = system(@cmd)
       }
     }
   }
 
-  if (defined $savedfd) {
-    dup2($savedfd->[1], $savedfd->[0]);
-  }
-
   if ($#cmds == -1) {
     return $ret;
-  }
-  
-  if ($cmds[0] eq "&&") {
+  } elsif ($cmds[0] eq "&&") {
     if ($ret != 0) { return $ret }
     return evalcmds(@cmds[1 .. $#cmds])
-  }
+  } elsif ($cmds[0] eq "\\\n") {
+    return evalcmds(@cmds[1 .. $#cmds])
+  } else {
+    die "unknown tail: @cmds";
+  } 
 }
 
 sub eval_copy {
